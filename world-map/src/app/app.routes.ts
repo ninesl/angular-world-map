8ab7c79faf46c46d347d4a19427232421ce1bee3
Lance Nines <lance.nines@gmail.com>
2024-05-03 15:13:07 -0400

    globe-map component styling

--- world-map/src/app/app.routes.ts
+++ world-map/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { GlobeMapComponent } from './globe-map/globe-map.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: 'globe', component: GlobeMapComponent}
+    { path: '', component: GlobeMapComponent} //blank is root route
+];
---
+++ world-map/src/app/globe-map/globe-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-globe-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './globe-map.component.html',
+  styleUrl: './globe-map.component.css'
+})
+export class GlobeMapComponent {
+
+}
